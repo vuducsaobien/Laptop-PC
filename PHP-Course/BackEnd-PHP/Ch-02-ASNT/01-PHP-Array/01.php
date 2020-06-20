557c20b07957f1a39e326304a51aa49956e26a6f
vuducsaobien <vuducsaobien95@gmail.com>
2020-06-20 16:35:47 +0700

    update Ch02 PHP ASNT 04:35 PM 20/06/2020

---
+++ PHP-Course/BackEnd-PHP/Ch-02-ASNT/01-PHP-Array/01.php
@@ -0,0 +1,15 @@
+	<?php
+        $n = 20;
+        $str = "PHP";
+
+        var_dump ($n);
+        echo "<br/>";
+        var_dump ($str);
+
+        echo "<br/>";
+        echo "<br/>";
+
+        echo gettype ($n);
+        echo "<br/>";
+        echo gettype ($str);
+	?>
