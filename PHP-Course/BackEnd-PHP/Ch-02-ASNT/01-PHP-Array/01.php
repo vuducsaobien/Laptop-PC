ab1a3da639b1f8a234c7c8eebc1629dc02e06143
vuducsaobien <vuducsaobien95@gmail.com>
2020-06-21 10:04:47 +0700

    update Lý thuyết Chương 01 PHP: Mảng Array

--- PHP-Course/BackEnd-PHP/Ch-02-ASNT/01-PHP-Array/01.php
+++ PHP-Course/BackEnd-PHP/Ch-02-ASNT/01-PHP-Array/01.php
@@ -1,13 +1,34 @@
-	<?php
-        $course   = array("PHP" , "Zend FrameWork" , "Joomla" );
+<?php
+//$student["SV001"] = array ("name" => "John", "sex" => 1, "score" => array(4,5,6));
+//$student["SV002"] = array ("name" => "Peter", "sex" => 1, "score" => array(5,6,7));
+//$student["SV003"] = array ("name" => "Marry", "sex" => 0, "score" => array(7,8,9));
 
-        echo "<pre>";
-        print_r ($course);
-        echo "</pre>";
+$student = array (
+        "SV001" => array("name"         => "John",
+                         "sex"          => 1,
+                         "score"        => array(4,5,6)
+                        ),
+        "SV002" => array("name"         => "Peter",
+                         "sex"          => 1,
+                         "score"        => array(5,6,7)
+                        ),
+        "SV003" => array("name"         => "Marry",
+                         "sex"          => 0,
+                         "score"        => array(7,8,9)
+                        ),
+                );
+        
+        
 
-        if (!empty ($course)) {
-                foreach ($course as $key => $value) {
-                        echo $value . "<br/>";
-                }
+
+if (!empty($student)) {
+        foreach ($student as $key => $value) {
+                $name = $value["name"];
+                $sex = ($value["sex"]==1) ? "Boy" : "Girl";
+
+                $score = array_sum($value["score"]);
+
+                echo "Name: " . $name . " - Sex: " .$sex . " - Total score: " . $score . "<br />";
         }
-	?>
+}
+?>
