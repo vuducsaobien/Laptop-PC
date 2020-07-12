956a4daf2df064a1b2e30f3454b4817bd939ca2b
vuducsaobien <vuducsaobien95@gmail.com>
2020-07-13 00:58:18 +0700

    1/ Sắp xếp & tối ưu lại mã nguồn Bt buổi - 08 2/ Sắp xếp lại lý thuyết buổi 08 - OOP còn thiếu 1 video Constant

--- tren-lop/buoi-08-API-coin-gold-xml/ly-thuyet/5-Phamvi & Su anh huong/3-Parent-Self/1-self/cat.class.php
+++ tren-lop/buoi-08-API-coin-gold-xml/ly-thuyet/5-Phamvi & Su anh huong/3-Parent-Self/1-self/cat.class.php
@@ -1,22 +1,19 @@
 <?php
 class Cat{
-
+    // 1
     //Properties
     public $name;
     public $color;
     public $age;
     public $weight;
-    static $maxSpeed = '20km/h';
-
+    // public $maxSpeed = '20km/h'; // 1
+    static public $maxSpeed = '20km/h'; // 2
 
     //Methods
 
-                            //Methods Construct
+    //Methods Construct
     //Cách 3: __construct() với tham số mặc định
     // $catB = new Cat();
-
-    
-
     public function __construct($name = 'Doraemon', $color = 'blue', $age = 10, $weight = '2kg'){
         $this->name     = $name;
         $this->color    = $color;
@@ -24,18 +21,16 @@ class Cat{
         $this->weight   = $weight;
     }
 
-
-
     public function showInfo(){
         echo '<br>Name: ' . $this->name;
         echo '<br>Color: ' . $this->color;
         echo '<br>Age: ' . $this->age;
         echo '<br>Weight: ' . $this->weight;
+        // echo '<br>max Speed: ' . $this->maxSpeed; // 1
+        echo '<br>max Speed self: ' . self::$maxSpeed; // 2 Cách 1
+        echo '<br>max Speed $this: ' . $this::$maxSpeed; // 2 Cách 2
+        echo '<br>max Speed Class: ' . Cat::$maxSpeed; // 2 Cách 3
 
-            //self ; Cat
-        echo '<br>maxSpeed - self: ' . self::$maxSpeed;
-        echo '<br>maxSpeed - Cat: ' . Cat::$maxSpeed;
-        
-        echo '<hr>';
+
     }
 }
--- tren-lop/buoi-08-API-coin-gold-xml/ly-thuyet/5-Phamvi & Su anh huong/3-Parent-Self/2/cat.class.php
+++ tren-lop/buoi-08-API-coin-gold-xml/ly-thuyet/5-Phamvi & Su anh huong/3-Parent-Self/2/cat.class.php
@@ -6,46 +6,37 @@ class Cat{
     public $color;
     public $age;
     public $weight;
-
-    // Static ở cat thì ở con lion ko hiển thị được
-    // static $maxSpeed = '20km/h';
-    public $maxSpeed = '20km/h';
-
-
+    // static public $maxSpeed = '20km/h'; // 1
+    /* - Static ở Properties ở lớp Cha (Cat) thì KHÔNG THỂ Override lên thuộc tính 
+    Properties ở lớp Con (Lion)
+       - Đối với Properties KHÔNG có khóa STATIC thì dùng SELF là KHÔNG ĐƯỢC
+     */
+    public $maxSpeed = '20km/h'; // 2
     //Methods
-
     //Methods Construct
-    public function __construct($name = 'Doraemon', $color = 'blue', $age = 10, $weight = '2kg'){
+    public function __construct($name = 'Doraemon', 
+                                $color = 'blue', 
+                                $age = 10, 
+                                $weight = '2kg'
+                                ){
         $this->name     = $name;
         $this->color    = $color;
         $this->age      = $age;
         $this->weight   = $weight;
     }
 
-    public function getName(){
-        return $this->name;
-    }
-
-    public function getColor(){
-        return $this->color;
-    }
-
-    public function getAge(){
-        return $this->age;
-    }
-
-    public function getWeight(){
-        return $this->weight;
-    }
-
-
     public function showInfo(){
         echo '<br>Name: ' . $this->name;
         echo '<br>Color: ' . $this->color;
         echo '<br>Age: ' . $this->age;
         echo '<br>Weight: ' . $this->weight;
+        // echo '<br>max Speed self: ' . self::$maxSpeed; // 1 Cách 1
+        // echo '<br>max Speed $this: ' . $this::$maxSpeed; // 1 Cách 2
+        // echo '<br>max Speed Class: ' . Lion::$maxSpeed; // 1 Cách 3
+        // echo '<br>max Speed Class: ' . Cat::$maxSpeed; // 1 Không hiển thị được
 
-        echo '<br>maxSpeed: ' . $this->maxSpeed;
-        //echo '<br>maxSpeed - Cat: ' . Cat::$maxSpeed;
+        // echo '<br>max Speed self: ' . self::$maxSpeed; // 2 Không sử dụng được
+        echo '<br>max Speed $this: ' . $this->maxSpeed; // 2
     }
+
 }
