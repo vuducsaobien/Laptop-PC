216fef26df666ef054ad9dd2c3ea1e2da61321f0
vuducsaobien <vuducsaobien95@gmail.com>
2020-07-14 15:42:42 +0700

    Xong BT-News T2, còn BT Mailer chưa hoàn thành

--- tren-lop/buoi-08-API-coin-gold-xml/lam-lai-BT-tren-lop/index.php
+++ tren-lop/buoi-08-API-coin-gold-xml/lam-lai-BT-tren-lop/index.php
@@ -1,43 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-	  <?php
-	  require_once 'define.php';
-	  require_once DIR_HTML .'head.php'; 
-	  ?>
+	  	<?php
+		// session_start();
+		// echo '<pre>';
+		// print_r($_SESSION);
+		// echo '</pre>';
+
+		require_once 'define.php';
+		require_once DIR_HTML .'head.php'; 
+		?>
   </head>
   <body>
-
-	<?php 
-		require_once DIR_HTML .'content.php';
-		require_once DIR_HTML . 'vnexpress.php';
-	?>
-
-		<div class="row">
-		<div class="col">
-		<div class="block-27">
-			<ul>
-			<li><a href="#">&lt;</a></li>
-			<li class="active"><span>1</span></li>
-			<li><a href="#">2</a></li>
-			<li><a href="#">3</a></li>
-			<li><a href="#">4</a></li>
-			<li><a href="#">5</a></li>
-			<li><a href="#">&gt;</a></li>
-			</ul>
-		</div>
-		</div>
+	<div id="colorlib-page">
+		<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+		<aside id="colorlib-aside" role="complementary" class="js-fullheight">
+		<div id="wrapper">
+    	<div class="title"></div>
+        <div id="form">
+			<?php   
+			echo '<button><a href='.DIR_HTML.'login.php>Login</a></button>';
+			?>		
 	</div>
 	</div>
-	<div class="col-xl-4 sidebar ftco-animate bg-light pt-5">
 
-	<?php 
-		require_once DIR_HTML . 'table-gold.php';
-		require_once DIR_HTML . 'table-coin.php'; 
-	?>
+		</aside> <!-- END COLORLIB-ASIDE -->
+		<div id="colorlib-main">
+		<section class="ftco-section ftco-no-pt ftco-no-pb">
+			<div class="container">
+				<div class="row d-flex">
+					<div class="col-xl-8 py-5 px-md-5">
+						<?php require_once DIR_HTML . 'vnexpress.php'; ?>
+					</div>
 
-	            </div>
-	          </div><!-- END COL -->
+					<div class="col-xl-4 sidebar ftco-animate bg-light pt-5">
+						<?php 
+							require_once DIR_HTML . 'table-gold.php';
+							require_once DIR_HTML . 'table-coin.php'; 
+						?>
+					</div>
+				</div><!-- END COL -->
 	    		</div>
 	    	</div>
 	    </section>
@@ -47,9 +49,6 @@
   <!-- loader -->
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
-	<?php
-	require_once DIR_HTML .'script.php'; 
-	?>
-    
+	<?php require_once DIR_HTML .'script.php'; ?>
   </body>
 </html>
