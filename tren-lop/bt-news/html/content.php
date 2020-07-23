cb106e90781eb0189d5c2f320688c769403d0064
vuducsaobien <vuducsaobien95@gmail.com>
2020-07-23 23:11:30 +0700

    xong Bt news

--- tren-lop/bt-news/html/content.php
+++ tren-lop/bt-news/html/content.php
@@ -1,80 +1,39 @@
 <?php
-/*
-$array1 = array(
-    "1" => "A",
-    "2" => "B",
-	"3" => "C",
-	"4" => "D"
-
-);
-
-$array2 = array(
-    "1" => "E",
-	"2" => "F",
-	"3" => "G",
-	"4" => "H",
-	"5" => "I"
-);
-
-foreach($array1 as $arr1) {
-    // print_r($arr1);
-    // echo "<br>";
-	$i = 0;
-    foreach($array2 as $arr2) {
-		if($i == 3) break;
-        print_r($arr2);
-		echo "<br>";
-		$i++;
-
-	}
-
-}
-die;
-
-
-foreach ($array as $item) {
-	echo "$item\n";
-	$array[] = $item;
-  }
-  print_r($array);
-die;
-*/
-
 	$rssTxt	= file_get_contents('data/rss.txt', true);
 	$url 	= preg_replace('/\s+/', '', $rssTxt);
 	$urlArr = explode('https', $url);
 	$a 		= array_shift($urlArr);
+	$xhtml 	= '';
+
 	foreach ($urlArr as &$b){
 		$b = 'https' . $b;
 	}
 	foreach ($urlArr as $value){
-		echo $value . '<hr>';
 		$xml   		= simplexml_load_file($value, 'SimpleXMLElement', LIBXML_NOCDATA);
 		$xmlArr 	= json_decode(json_encode($xml), 1);
 		$channel 	= $xmlArr['channel'];
 		$item 		= $channel['item'];
-		echo '<pre>';
-		print_r($item);
-		echo '</pre>';
-		$xhtml 		= '';
-		if(strpos($url, 'tuoitre') !== false) {
-				$danhMuc 	= substr($channel['title'],0,-11);
-		} else { $danhMuc 	= substr($channel['title'],0,-3); }
 
 		$i 			= 0;
 		foreach ($item as $description){
-			if($i == 2) { break 1;}
+			if($i == 2) break;
+			
 			$title = $description['title'];
 			$link  = $description['link'];
 			$day   = $description['pubDate'];
 
-			if(strpos($url, 'tuoitre') !== false) {
+			if(strrpos($link, 'vnexpress') === false) {
 				$pregMatch = '#.*<img src="(.*)" /></a>(.*)<end>#imSU';
-			} else { $pregMatch = '#.*<img src="(.*)" ></a></br>(.*)<end>#imSU'; }
+				$danhMuc 	= substr($channel['title'],0,-11);
+			} else { 
+				$pregMatch = '#.*<img src="(.*)" ></a></br>(.*)<end>#imSU'; 
+				$danhMuc 	= substr($channel['title'],0,-3);
+			}
 		
 			preg_match_all($pregMatch, $description['description'] . '<end>', $matches);
 			$srcImage    = implode($matches[1]);
 			$description = implode($matches[2]);
+
 			$xhtml 		.= 	
 			'<div class="row pt-md-4">
 				<div class="col-md-12">
@@ -97,5 +56,5 @@ die;
 			$i++;
 		}
 	}
-?>
+	?>
 
